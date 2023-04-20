68ea2fd894ff92cc6314a1778ca0d4adf7255fc7
Luana Mendes <92415603+luuanamendes@users.noreply.github.com>
2023-04-20 14:15:13 -0300

    Update localdb.js

--- localdb.js
+++ localdb.js
@@ -1,4 +1,7 @@
 const db = {
-Henrique: { chunks: ['Hen', 'ri', 'que'], phones: ['EN', 'RI', 'KE'] },
+  Henrique: { chunks: ['Hen', 'ri', 'que'], phones: ['EN', 'RI', 'KE'] },
+  Luana: { chunks: ['Lu', 'a', 'na'], phones: ['L', 'AH', 'N'] },
+  luana: { chunks: ['Lu', 'a', 'na'], phones: ['L', 'AH', 'N'] },
+  LUANA: { chunks: ['Lu', 'a', 'na'], phones: ['L', 'AH', 'N'] },
 };
 export default db;
